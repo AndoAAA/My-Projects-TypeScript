b00accd74b715d980ca8a8f88dff1c0391a70928
AndoAAA <tarverdyan070@gmail.com>
2025-03-13 02:53:23 +0400

    fix

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,9 @@
-import { ReportHandler } from 'web-vitals';
+import { reportWebVitals } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitalsHandler = (onPerfEntry: any) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    onPerfEntry(reportWebVitals);
   }
 };
 
-export default reportWebVitals;
+export default reportWebVitalsHandler;
